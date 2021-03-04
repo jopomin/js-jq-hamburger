54e1cd9b363a36925bb9a98091c2a955939c221e
Giovanni <giovannipor23@gmail.com>
2021-03-04 16:44:25 +0100

    *COMPLETO* - Esercizio base

---
+++ main.js
@@ -0,0 +1,11 @@
+$(".header-right > a").click(
+    function() {
+        $(".hamburger-menu").addClass("active");
+    }
+)
+
+$(".hamburger-menu .close").click(
+    function() {
+    $(".hamburger-menu").removeClass("active");
+}
+)
